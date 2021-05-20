057a0e81387d5aec77063e3781d5264f5bfba328
yuichi oyama <yuichi19991208@gmail.com>
2021-05-21 08:53:08 +0900

    practice function

--- lesson2.py
+++ lesson2.py
@@ -19,9 +19,55 @@ print('hello. \nHello are you?')
 print(r'c:\name\name')
 
 print("###################")
-print("""
+print("""\
 line
 line
-line
+line\
 """)
 print("#######################")
+
+print('hi.' * 3 + 'mike.')
+print('py' + 'thon.')
+print('py' 'thon')
+py = 'py'
+
+print(py + 'thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')
+
+print(s)
+
+word = 'python'
+print(word[2])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print('##########################')
+print(word[0:2])
+print(word[:2])
+print('##########################')
+print(word[2:])
+print('##########################')
+word = 'j' + word[1:]
+print(word)
+print(word[:])
+print(len(word))
+
+s = 'My name is Mike. Hi Mike.'
+print(s)
+is_start = s.startswith('My')
+print((is_start))
+is_start = s.startswith('x')
+print((is_start))
+
+print('##########################')
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
