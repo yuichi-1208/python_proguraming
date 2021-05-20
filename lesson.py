b7c53f3224d65f6723bba396f4a8e4884ba9cc6a
yuichi oyama <yuichi19991208@gmail.com>
2021-05-21 08:14:03 +0900

    first commit

---
+++ lesson.py
@@ -0,0 +1,20 @@
+print('hello world')
+
+a = 'test'
+b = a
+c = b
+
+print(c)
+
+num = 1
+name = '1'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+new_num = int(name)
+
+print(new_num, type(new_num))
+
---
+++ lesson2.py
@@ -0,0 +1,27 @@
+import math
+
+print('he', 'mike', sep=',', end='\n')
+print('he', 'mike', sep=',', end='\n')
+
+
+
+result = math.sqrt(25)
+print(result)
+
+y = math.log2(10)
+print(y)
+
+print('hello')
+print("hello")
+print("I don't know")
+print('I don\'t know')
+print('hello. \nHello are you?')
+print(r'c:\name\name')
+
+print("###################")
+print("""
+line
+line
+line
+""")
+print("#######################")
